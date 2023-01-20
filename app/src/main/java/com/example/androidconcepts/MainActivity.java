12edf3cb2b5e3aa4e202860031749efa497d6168
Armando Salazar <armandosalazar351@gmail.com>
2023-01-19 23:15:37 -0600

    Add onClick from xml

--- app/src/main/java/com/example/androidconcepts/MainActivity.java
+++ app/src/main/java/com/example/androidconcepts/MainActivity.java
@@ -4,23 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private final String TAG = "MainActivity";
     private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "Hello World");
         button = findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                openSecondActivity();
-            }
-        });
+        button.setOnClickListener(view -> openSecondActivity());
     }
 
     void openSecondActivity() {
--- app/src/main/java/com/example/androidconcepts/SecondActivity.java
+++ app/src/main/java/com/example/androidconcepts/SecondActivity.java
@@ -3,13 +3,20 @@ package com.example.androidconcepts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class SecondActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        this.getSupportActionBar().hide();
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
-        this.getSupportActionBar().hide();
+    }
+
+    public void goToMainActivity(View view) {
+        Log.d("[<<<<<<<>>>>>>]", "Home");
     }
 }
