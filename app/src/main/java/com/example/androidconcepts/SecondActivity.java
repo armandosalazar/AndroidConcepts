df9f81fe42a9011ee2ea7dd95d40bb656193a279
Armando Salazar <armandosalazar351@gmail.com>
2023-01-20 23:06:13 -0600

    Clear SecondActivity

--- app/src/main/java/com/example/androidconcepts/SecondActivity.java
+++ app/src/main/java/com/example/androidconcepts/SecondActivity.java
@@ -1,22 +1,16 @@
 package com.example.androidconcepts;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class SecondActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        this.getSupportActionBar().hide();
+        // this.getSupportActionBar().hide();
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
-    }
-
-    public void goToMainActivity(View view) {
-        Log.d("[<<<<<<<>>>>>>]", "Home");
     }
 }
